93dd08c346adabe55125cfb85cd65489eeeafaed
TDave <116151027+TDavid555@users.noreply.github.com>
2024-01-11 14:02:30 +0100

    Add files via upload

---
+++ utijava.js
@@ -0,0 +1,15 @@
+function funkcio1(){
+
+let uzemfajta = document.getElementById("uzemanyagok").value;
+
+if (uzemfajta=="Benzin") {
+    document.getElementById().innerHTML=
+    
+}
+
+
+
+
+
+
+}
